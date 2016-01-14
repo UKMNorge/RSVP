1e19e9f1c6340f6f35743fbeaa4be6d17be2695f
Marius Mandal <mariusmandal@gmail.com>
2016-01-14 23:05:36 +0100

    Attending-images

--- src/UKMNorge/DipBundle/Entity/User.php
+++ src/UKMNorge/DipBundle/Entity/User.php
@@ -487,7 +487,7 @@ class User implements UserInterface
         return $this->gender;
     }
     
-    public function getThumb() {
+    public function getThumbnail() {
 	    return '//graph.facebook.com/'.$this->getFacebookId() .'/picture';
     }
     
