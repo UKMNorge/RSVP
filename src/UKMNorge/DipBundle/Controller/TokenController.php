d33cd8ed8a05c28223d455d138dff716d6b543b8
Asgeir S. Hustad <askhus@online.no>
2016-01-14 17:38:59 +0000

    Fikset redirect ved reentry.

--- src/UKMNorge/DipBundle/Controller/TokenController.php
+++ src/UKMNorge/DipBundle/Controller/TokenController.php
@@ -44,13 +44,21 @@ class TokenController extends Controller
     	// Her sjekker vi om brukeren har en session med en autentisert token, 
     	// og hvis ikke genererer vi en og sender brukeren videre til Delta.
 
-        // Registrer hvilken side brukeren var på
+        // Registrer hvilken side brukeren var på i 
         $previous = $request->headers->get('referer');
-        $uri = parse_url($previous);
-        var_dump($uri);
-        var_dump($request);
-        throw new Exception('Staaaahp!');
-    	// Send request to Delta with token-info
+        if($previous) {
+            $uri = parse_url($previous);
+            if ($uri['host'] == ($this->getParameter('dip_location').'.'.$this->getParameter('UKM_HOSTNAME'))) {
+                $session = $request->getSession();
+                $session->set('referer', $uri['path']);
+            }
+            // var_dump($uri);
+            // var_dump(($this->getParameter('dip_location').$this->getParameter('UKM_HOSTNAME')));
+            // throw new Exception('Staaaaahp');
+        }
+        // Om lokal side vil kan de sende brukeren tilbake til den siden når brukeren kommer tilbake fra Delta.
+
+        // Send request to Delta with token-info
     	// $dipURL = 'http://delta.ukm.dev/web/app_dev.php/dip/token';
         #$location = 'ambassador';
         $location = $this->container->getParameter('dip_location');
