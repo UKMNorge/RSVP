cf3e16d616211bda83690fcb52074f3a037c19ea
Marius Mandal <mariusmandal@gmail.com>
2016-01-14 20:39:53 +0100

    Send SMS-funksjoner

--- src/UKMNorge/RSVPBundle/Services/ResponseService.php
+++ src/UKMNorge/RSVPBundle/Services/ResponseService.php
@@ -23,15 +23,33 @@ class ResponseService {
 	}
 	
 	public function getCountYes( $event ) {
-		return $this->getCount( $event, 'yes' );;
+		return $this->getCount( $event, 'yes' );
 	}
 
 	public function getCountNo( $event ) {
-		return $this->getCount( $event, 'no' );;
+		return $this->getCount( $event, 'no' );
 	}
 
 	public function getCountMaybe( $event ) {
-		return $this->getCount( $event, 'maybe' );;
+		return $this->getCount( $event, 'maybe' );
 	}
 
+	public function alertUserPromoted( $user, $event ) {
+		$data = array();
+
+		$data['id'] = $event->getId();
+		$data['name'] = $this->getName( $event );
+		$data['response'] = 'no';
+
+		$link = $this->router->generate('ukmrsvp_event_response', 'no');
+		
+		$message = 'Noen har meldt seg av '. $event->getName() .' og du er nå påmeldt! Hvis du ikke kan komme, klikk på lenken: '. $link;
+		
+		$UKMSMS = $this->container->get('ukmsms');
+		try {
+		    $UKMSMS->sendSMS( $user->getPhone(), $message );
+		} catch( Exception $e ) {
+			mail('support@ukm.no', 'RSVP ERROR', 'Denne SMS ble ikke sendt: '. $message);
+		}
+	}
 }
