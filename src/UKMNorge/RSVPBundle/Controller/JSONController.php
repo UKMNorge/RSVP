6bf1e39e4f2bf128a216c7acc4433be4987bc57e
Marius Mandal <mariusmandal@gmail.com>
2018-03-19 08:01:01 +0100

    JSON export event data

---
+++ src/UKMNorge/RSVPBundle/Controller/JSONController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace UKMNorge\RSVPBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use UKMNorge\RSVPBundle\Entity\Response;
+use UKMNorge\RSVPBundle\Entity\Waiting;
+use Exception;
+
+class JSONController extends Controller
+{
+	public function listAction()
+	{
+		$eventServ = $this->get('ukmrsvp.event');
+		
+		$eventData = [];
+		foreach( $eventServ->getAll() as $event ) {
+
+			
+			$eventData[] = $event->expose();
+		}
+
+		return new JsonResponse( $eventData );
+	}
+}
